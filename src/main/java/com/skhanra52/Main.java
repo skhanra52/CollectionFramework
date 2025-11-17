b41890fbc1504add0fea9db2f6898af230113fca
Suman Kalyan Khanra <skhanra52@gmail.com>
2025-11-17 16:03:51 +0530

    Collection class methods

--- src/main/java/com/skhanra52/Main.java
+++ src/main/java/com/skhanra52/Main.java
@@ -1,9 +1,6 @@
 package com.skhanra52;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -59,9 +56,6 @@ public class Main {
             that will print "Current deck"
             as description, and use 4, as the number of rows to be printed.
      */
-
-        List<Card> deck = Card.getStandardDeck();
-        Card.printDeck(deck);
 
         Card[] cardArray = new Card[13];
         Card aceOfHeart = Card.getFaceCard(Suit.HEART,'A');
@@ -124,6 +118,145 @@ public class Main {
         // If you want to get a true copy then we have to use List.copyOf() method, but this is immutable.
         cards = List.copyOf(kingsOfClubs);
         Card.printDeck(cards, "List copies of kings", 1);
+        // Collections.shuffle() method------------------------------------------------------------------
+        // Deck contain the standard cards of 52
+        List<Card> deck = Card.getStandardDeck();
+        Card.printDeck(deck);
+        // We can use random to shuffle cards. However, we can use Collections.shuffle() method as well.
+        // This is useful method if you are writing a card game certainly, if you ever want to randomize
+        // with the existing elements.
+//        Collections.shuffle(deck);
+//        Card.printDeck(deck, "Shuffle cards", 4);
+        // Collections.reverse() method-------------------------------------------------------------------
+        Collections.reverse(deck);
+        Card.printDeck(deck,"Reversed Deck", 4);
+        // Collections.sort() method---------------------------------------------------------------------
+        // There are two flavours to it, much like list interface's sort() method. One requires elements
+        // in the list to implement comparable, and one does not.
+        // Here, the sort() method which does not need comparable.
+        // Collections.sort() method takes the list as first argument. If the element implements comparable
+        // that would be it, but if they don't or if we want to force alternative sort, we can pass comparator.
+        // We can set up comparator as local variable so that we can reuse it.
+        // Collections.sort() used in the legacy code. In the new codes we can use list.sort() method.
+       Comparator<Card> sortingAlgorithm  = Comparator.comparing(Card::rank)
+                .thenComparing(Card::suit);
+        Collections.sort(deck, sortingAlgorithm);
+        Card.printDeck(deck, "sorted deck of cards", 13);
+        //-Collection.subList() method-----------------------------------------------------------------------------
+        // Compare sublist to full list, we will carve out few smaller list from the standard list using subList()
+        // and passing them to new ArrayList constructor.
+        List<Card> threes = new ArrayList<>(deck.subList(4,8));
+        Card.printDeck(threes, "Sublist of Threes", 1);
+        //Collection.indexOfSubList()-------------------------------------------------------------------------------
+        // Collection.indexOfSubList() will take list as first argument in which the sublist should be found, and
+        // subList in the 2nd element. This provides the starting index of the sublist in the original list.
+        int subListIndex = Collections.indexOfSubList(deck, threes);
+        System.out.println("sublist index for threes "+subListIndex);
+        //deck.containsAll()----------------------------------------------------------------------------------------
+        // deck.containsAll() method will return true if the parent list contains all the element of sublist.
+        // This is the legacy method, we can use list.containsAll() method.
+        // we can wrap the code in HashSet constructor new HashSet<>(deck).containsAll(threes) as well.
+        System.out.println(deck.containsAll(threes));
+
+        // Collections.disjoint() method , it takes two list of argument and compares them, if any one of the element
+        // of the second list found in the first list then it will return false, means it has element in common.
+        boolean disjoint = Collections.disjoint(deck,threes);
+        System.out.println("disjoint "+disjoint);
+
+        // Like the Arrays class that gave us a binarySearch for the arrays, Collections class supports a binary search
+        // for the lists.
+        // Condition 1: Both method require element to be sorted first and neither guarantees what index is returned
+        //              if there is duplicates.
+        // Condition 2: The binarySearch() method in Collections is overloaded like sort() method.
+        //              We can pass a comparable elements, or we can pass a list of separate comparator, which will
+        //              match the way elements are sorted.
+        // Condition 3: Both methods takes an element to be searched. The element should be same type as the elements
+        //              in the list being searched.
+
+        Card tenOfHearts = Card.getNumericCard(Suit.HEART, 10);
+        deck.sort(sortingAlgorithm); // must be sorted, here we are using list.sort() method not collection as line 144.
+        int foundIndex = Collections.binarySearch(deck, tenOfHearts, sortingAlgorithm);
+        System.out.println("Found index "+foundIndex);
+        System.out.println(deck.get(foundIndex));
+        // We can use the same using list's indexOf() method
+        int findIndexOfList = deck.indexOf(tenOfHearts);
+        System.out.println("FindIndexOfList "+findIndexOfList );
+        //------------------------------------------------------------------------------------------------
+        // Collections.replaceAll(), it takes first argument a list, second argument as which has to be replaced,
+        // third argument by which element. Meaning, second will be replaced by third args.
+        /*
+            java.util.Collections
+            @Contract(mutates = "param1")
+            public static <T> boolean replaceAll(
+                @NotNull   java.util.List<T> list,
+                @Nullable   T oldVal,
+                @Nullable   T newVal
+                )
+            Replaces all occurrences of one specified value in a list with another. More formally,
+            replaces with newVal each element e in list such that (oldVal==null ? e==null : oldVal.equals(e)).
+            (This method has no effect on the size of the list.)
+            Params:
+            list – the list in which replacement is to occur.
+            oldVal – the old value to be replaced.
+            newVal – the new value with which oldVal is to be replaced.
+            Type parameters:
+            <T> – the class of the objects in the list
+            Returns:
+            true if list contained one or more elements e such that (oldVal==null ? e==null : oldVal.equals(e)).
+            Throws:
+            UnsupportedOperationException – if the specified list or its list-iterator does not support
+                                            the set operation.
+            Since: 1.4
+         */
+
+        Card tenOfClubs = Card.getNumericCard(Suit.CLUB, 10);
+        System.out.println(tenOfClubs +" index "+ deck.indexOf(tenOfClubs) + " and "
+                + tenOfHearts + "index "+deck.indexOf(tenOfHearts));
+        Card.printDeck(deck, "Display deck", 4);
+        boolean replaced =  Collections.replaceAll(deck,tenOfHearts,tenOfClubs);
+        Card.printDeck(deck,"Replaced tenOfHearts by tenOfClubs", 4);
+//        if(replaced){
+//            System.out.println("The card is replaced");
+//        }else {
+//            System.out.println("The card is not replaced");
+//        }
+        // or
+        System.out.println(replaced? "Card is replaced": "Card is not replaced");
+        //Collection.frequency() method----------------------------------------------------------------------------
+        System.out.println("Ten of hearts cards = " +Collections.frequency(deck,tenOfHearts)); // 0 because replaced
+        System.out.println("Ten of club cards = "+Collections.frequency(deck,tenOfClubs)); // output 2
+        // Collections.min()/Collections.max() method -------------------------------------------------------------
+        /*
+        Collections class provides us min() and max() method returns the minimum and maximum element in the collection
+        respectively based on the natural ordering of the elements or a provided comparator.
+        Like sort() and binarySearch() method we can use one of the overloaded versions,
+        -> The first if the class implements comparable, which we are not doing in this example.
+        -> Second, takes the comparator. We will use the same here for the Card example by passing the sortingAlgorithm
+        variable.
+         */
+        System.out.println("Best card "+Collections.max(deck, sortingAlgorithm));
+        System.out.println("Min card "+Collections.min(deck,sortingAlgorithm));
+
+        //Collections.rotate()-----------------------------------------------------------------------------------
+         Comparator<Card> sortBySuit = Comparator.comparing(Card::suit)
+                 .thenComparing(Card::rank);
+         List<Card> standardDeck = Card.getStandardDeck();
+         Card.printDeck(deck,"normal deck", 4);
+         Collections.sort(standardDeck,sortBySuit); // Legacy method, now we can use standardDeck.sort(sortBySuit)
+         Card.printDeck(standardDeck,"Sort by Suit", 4);
+         List<Card> copied = new ArrayList<>(standardDeck.subList(0,13));
+         Card.printDeck(copied, "Copied card", 1);
+         Collections.rotate(copied, 5);
+         Card.printDeck(copied,"Rotate standardDeck", 1);
+         // Collections.swap()-----------------------------------------------------------------------------------
+        copied = new ArrayList<>(deck.subList(0,13));
+        for (int i=0;i<copied.size();i++){
+            Collections.swap(copied,i, copied.size() - 1 - i);
+        }
+        Card.printDeck(deck,"swap implementation", 4);
+
+
+
 
 
     }
