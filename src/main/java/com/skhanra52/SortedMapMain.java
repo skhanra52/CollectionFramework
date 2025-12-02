5558026f6e5711e27b61a3e99e82110ce9e346f9
Suman Kalyan Khanra <skhanra52@gmail.com>
2025-12-02 06:37:00 +0530

    TreeMap example

--- src/main/java/com/skhanra52/SortedMapMain.java
+++ src/main/java/com/skhanra52/SortedMapMain.java
@@ -1,19 +1,56 @@
 package com.skhanra52;
 
-public class SortedMapMain {
+import java.time.LocalDate;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.NavigableMap;
+import java.util.TreeMap;
 
-    public static void main(String[] args) {
-    /*
-    Order and sorted Map implementation:-------------------------------------
+ /*
+    Ordered and sorted Map implementation:-------------------------------------
     -> The Map interface has the LinkedHashMap and TreeMap classes.
-    -> The LinkedHashMap is a key value entry collection whose keys are ordered by insertion order.
+    -> The LinkedHashMap is a key/value entry collection whose keys are ordered by insertion order.
     -> The TreeMap is sorted by its keys, so a key needs to implement comparable,
        or be initialized with a specific comparator.
 
-     Created a class called Student,
+     Created a class called Student, see further over there.
      */
 
+public class SortedMapMain {
 
 
+    public static Map<String, Purchase> purchases = new LinkedHashMap<>();
+    // NavigableMap will give all the method access of NavigableMap and the SortedMap like Set.
+    public static NavigableMap<String, Student> students = new TreeMap<>();
+
+
+    public static void main(String[] args) {
+        Course jmc = new Course("jmc101", "Java Master Class", "Java");
+        Course python = new Course("jmc102", "Python Master Class", "Python");
+        addPurchase("Mary Martin",jmc, 129.99);
+        addPurchase("Andy Martin", python, 139.99);
+        addPurchase("Mary Martin",python, 149.99);
+        addPurchase("Joe Jones",jmc, 149.99);
+        addPurchase("Bill Brown",python, 119.99);
+
+        purchases.forEach((k,v) -> System.out.println(k+" "+v));
+        students.forEach((k,v) -> System.out.println(k +" "+v) );
+
+    }
+
+    private static void addPurchase(String name, Course course, double price){
+        Student existingStudent = students.get(name);
+        if(existingStudent == null) {
+            existingStudent = new Student(name,course);
+            students.put(name,existingStudent);
+        }else{
+            existingStudent.addCourse(course);
+        }
+
+        int day = purchases.size() + 1;
+        String key = course.courseId() + "_" + existingStudent.getId();
+        int year = LocalDate.now().getYear();
+        Purchase purchase = new Purchase(course.courseId(), existingStudent.getId(), price, year, day);
+        purchases.put(key, purchase);
     }
 }
--- src/main/java/com/skhanra52/Student.java
+++ src/main/java/com/skhanra52/Student.java
@@ -1,11 +1,15 @@
 package com.skhanra52;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 record Course(String courseId, String name, String subject){}
 record Purchase(String courseId, int studentIs, double price, int yr, int dayOfYear ){
     // Yr is the date of purchase and dayOfYear from 1-365.
 
+    // will be used as key in the upcoming maps.
     public LocalDate purchaseDate() {
         return LocalDate.ofYearDay(yr,dayOfYear);
     }
@@ -13,4 +17,39 @@ record Purchase(String courseId, int studentIs, double price, int yr, int dayOfY
 }
 
 public class Student {
+    public static int lastId = 1;
+    private String name;
+    private int id;
+    private List<Course> courseList;
+
+    public Student(String name, List<Course> courseList) {
+        this.name = name;
+        this.courseList = courseList;
+        id = lastId++;
+    }
+
+    public Student(String name, Course course) {
+        this(name, new ArrayList<>(List.of(course)));
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void addCourse(Course course){
+        courseList.add(course);
+    }
+
+    @Override
+    public String toString() {
+        String[] courseName = new String[courseList.size()];
+        // definition of Arrays.SetAll(): public static <T> void setAll(T[] array, IntFunction<? extends T> generator)
+        // Actually its creating a local array which will contains ony course name, no course id or subject
+        Arrays.setAll(courseName, i -> courseList.get(i).name());
+        return "[%d] : %s".formatted(id,String.join(", ",courseName));
+    }
 }
